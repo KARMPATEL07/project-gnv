afe8080d363d4ee6ea66d37d959e2f8f78a95aa4
KARMPATEL07 <karmpatel432.pf@gmail.com>
2024-03-31 01:27:39 +0530

    pillars page done finishine remaining

--- frontend/src/components/cards/Person.jsx
+++ frontend/src/components/cards/Person.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
+// import React from 'react';
 import '../../styles/person.css';
 
-const Person = ({ name, position, imageUrl }) => {
+// const Person = ({ name, position, imageUrl }) => {
+//     return (
+//         <div className="person-container">
+//             {/* <div className="oval-animation1"></div>
+//             <div className="oval-animation2"></div> */}
+//             <img src={imageUrl} alt={name} className="person-image" />
+//             <div className="person-details-container">
+
+//                 <div className="person-details">
+//                     <div className="p-name">
+//                         <h2>{name}</h2>
+//                     </div>
+//                     <div className="p-position">
+//                         <p>{position}</p>
+//                     </div>
+
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Person;
+import React from 'react';
+// import './ProfileCard.css'; // Styles for the profile card
+
+const ProfileCard = ({ name, position, imageUrl }) => {
     return (
-        <div className="person-container">
-            <div className="oval-animation1"></div>
-            <div className="oval-animation2"></div>
-            <img src={imageUrl} alt={name} className="person-image" />
-            <div className="person-details-container">
-
-                <div className="person-details">
-                    <div className="p-name">
-                        <h2>{name}</h2>
-                    </div>
-                    <div className="p-position">
-                        <p>{position}</p>
-                    </div>
-
+        <div className="profile-wrapper">
+            <div className="profile-card">
+                <div className="profile-ring"></div>
+                <div className="profile-img">
+                    <img src={imageUrl} alt={name} />
+                </div>
+                <div className="profile-details">
+                    <h2>{name}</h2>
+                    <p>{position}</p>
+                    <div className="overlay"></div>
                 </div>
             </div>
+            
+            
         </div>
     );
 };
 
-export default Person;
+export default ProfileCard;
